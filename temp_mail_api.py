3556b68bf470b6a0658e83c13e4406f40a6a0251
brend3n <brenden@knights.ucf.edu>
2022-01-03 12:20:55 -0500

    All functions work with TempMail class conversion

--- temp_mail_api.py
+++ temp_mail_api.py
@@ -19,11 +19,11 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 class TempMail():
 	start_url = "https://mytemp.email/2/"
 	def __init__(self):
-		self.start()
+		self.start(self.start_url)
 	
 	def start(self, url):
 		chrome_options = Options()
-		chrome_options.add_argument("--headless")
+		# chrome_options.add_argument("--headless")
 		
 		# Remove the warnings
 		try:
@@ -49,7 +49,7 @@ class TempMail():
 	def get_email_address(self):
 		data = self.driver.execute_script("return window.localStorage")
 		self.inbox, self.hash = self.clean_email_address_GET(data)
-		self.email_url = f"https://mytemp.email/2/#!/inbox/{inbox}/{hash}"
+		self.email_url = f"https://mytemp.email/2/#!/inbox/{self.inbox}/{self.hash}"
 		print(f"email_url: {self.email_url}")
 		return self.email_url, self.inbox, self.hash
 
@@ -63,106 +63,32 @@ class TempMail():
 
 	def get_message(self):
 		inbox = [] 
-		start_url = f"https://mytemp.email/2/#!/inbox/{self.address}/{self.hash}"
 		data = self.read_XPATH(xpath='//*[@id="app"]/div/md-content/div/div/div/md-list/md-list-item[1]/a')
-		
 		message_url = f"https://mytemp.email/2/{data}"
-		# print(f"message_url: {message_url}")
 		self.driver.get(message_url)
-		
 		sleep(5)
-		
 		message = self.driver.find_element(By.XPATH, '//*[@id="eml-part-text"]/pre').get_attribute('innerHTML')
-		
 		print(f"message: {message}")
+  
 		return message
-	def delete_message(self):
-		start_url = f"https://mytemp.email/2/#!/inbox/{address}/{hash}"
-		# driver = start(start_url)
-		data = self.read_XPATH(xpath='//*[@id="app"]/div/md-content/div/div/div/md-list/md-list-item[1]/a')
 
-		message_address = data[7:].split("/")
-		# print(f"message_address: {message_address}")
-		delete_url = f"https://api.mytemp.email/1/eml/destroy?eml={message_address[0]}&hash={message_address[1]}"
-		# print(f"delete_url: {delete_url}")
-		output = requests.get(delete_url)
-		# print(f"output: {output}")
+	def get_messages(self):
 		pass
 
+	def delete_message(self):
+		self.driver.get(self.email_url)
+		data = self.read_XPATH(xpath='//*[@id="app"]/div/md-content/div/div/div/md-list/md-list-item[1]/a')
+		message_address = data[7:].split("/")
+		print(f"message_address: {message_address}")
+		delete_url = f"https://api.mytemp.email/1/eml/destroy?eml={message_address[0]}&hash={message_address[1]}"
+		print(f"delete_url: {delete_url}")
+		output = requests.get(delete_url)
+		print(f"Request status: {output}")
 
-# ! Code below works
-'''
-def start(url):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    
-    # Remove the warnings
-    try:
-        driver = webdriver.Chrome(chrome_options=chrome_options)
-    except Exception as e:
-        pass
-    driver.get(url)
-    sleep(5)
-    return driver
-
-def clean_email_address_GET(data):
-    
-	data = str(data)[97:]
-	data = data[:data.index("',") + 1].strip()
-	data = data.replace(':{"',':"').replace("}}", "}")
-	data = list(data[data.index(":"):])
-	data[0] = "{"
-	data = "".join(data)[:-1]
-	
-	data = json.loads(data)
-	inbox = data["inbox"]
-	hash = data["hash"]
-	return inbox, hash
-
-def get_email_address(driver):
-	data = driver.execute_script("return window.localStorage")
-	inbox,hash = clean_email_address_GET(data)
-	email_url = f"https://mytemp.email/2/#!/inbox/{inbox}/{hash}"
-	print(f"email_url: {email_url}")
-	return email_url, inbox, hash
-
-def read_XPATH(driver, xpath):
-	try:
-		WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-	except Exception as e:
-		print(f"Caught Exception: {e}")
-	data = driver.find_element(By.XPATH, xpath).get_attribute("ng-href");
-	# print(f"data: {data}")
-	return data
-
-def get_message(driver, address, hash):
-    inbox = []
-    
-    start_url = f"https://mytemp.email/2/#!/inbox/{address}/{hash}"
-    # driver = start(start_url)
-    data = read_XPATH(driver, xpath='//*[@id="app"]/div/md-content/div/div/div/md-list/md-list-item[1]/a')
-    
-    message_url = f"https://mytemp.email/2/{data}"
-    # print(f"message_url: {message_url}")
-    driver.get(message_url)
-    
-    sleep(5)
-    
-    message = driver.find_element(By.XPATH, '//*[@id="eml-part-text"]/pre').get_attribute('innerHTML')
-    
-    print(f"message: {message}")
-    return message
-
-def delete_message(driver):
-	start_url = f"https://mytemp.email/2/#!/inbox/{address}/{hash}"
-	# driver = start(start_url)
-	data = read_XPATH(driver, xpath='//*[@id="app"]/div/md-content/div/div/div/md-list/md-list-item[1]/a')
-
-	message_address = data[7:].split("/")
-	# print(f"message_address: {message_address}")
-	delete_url = f"https://api.mytemp.email/1/eml/destroy?eml={message_address[0]}&hash={message_address[1]}"
-	# print(f"delete_url: {delete_url}")
-	output = requests.get(delete_url)
-	# print(f"output: {output}")
-	pass
-'''
+def main():
+    mail = TempMail()
+    mail.get_email_address()
+    mail.get_message()
+    mail.delete_message()
+if __name__ == '__main__':
+    	main()
